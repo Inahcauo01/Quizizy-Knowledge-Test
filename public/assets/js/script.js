55def6766b941113858f2901b58b7b3c02d32324
tarek <tarek.ouachani01@gmail.com>
2022-12-20 10:35:07 +0100

    create stepper componnent using JS

---
+++ public/assets/js/script.js
@@ -0,0 +1,62 @@
+
+let start = document.querySelector(".start")
+let next = document.querySelector(".next");
+let previous =  document.querySelector(".previous");
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let steps = document.querySelectorAll(".step");
+let iconStep1 = document.querySelector(".icon-step1")
+let iconStep2 = document.querySelector(".icon-step2")
+let iconStep3 = document.querySelector(".icon-step3")
+let iconStep4 = document.querySelector(".icon-step4")
+
+start.classList.add("hide")
+iconStep1.classList.add("color")
+//stepper next
+next.addEventListener("click",()=>{
+    previous.classList.remove("hide");
+
+    if(!one.classList.contains("hide")){
+        steps.forEach(step => { step.classList.add("hide"); });
+        two.classList.remove("hide");
+        iconStep2.classList.add("color")
+    }
+    else if(!two.classList.contains("hide")){
+        steps.forEach(step => { step.classList.add("hide"); });
+        three.classList.remove("hide");
+        iconStep3.classList.add("color")
+    }
+    else if(!three.classList.contains("hide")){
+        steps.forEach(step => { step.classList.add("hide"); });
+        four.classList.remove("hide");
+        iconStep4.classList.add("color")
+
+        next.classList.add("hide");
+        start.classList.remove("hide");
+    }
+})
+//stepper previous
+previous.addEventListener("click",()=>{
+
+    if(!two.classList.contains("hide")){
+        console.log("1")
+        steps.forEach(step => { step.classList.add("hide"); });
+        one.classList.remove("hide");
+        previous.classList.add("hide");
+        iconStep2.classList.remove("color")
+    }
+    else if(!three.classList.contains("hide")){
+        console.log("2")
+        steps.forEach(step => { step.classList.add("hide"); });
+        two.classList.remove("hide");
+        iconStep3.classList.remove("color")
+    }
+    else if(!four.classList.contains("hide")){
+        console.log("3")
+        steps.forEach(step => { step.classList.add("hide"); });
+        three.classList.remove("hide");
+        iconStep4.classList.remove("color")
+    }
+})
